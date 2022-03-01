45919f5a583b71a6bbf2b40fcf74b0d25228d795
H4rnas <M.pozaroszczyk@outlook.com>
2022-03-01 13:00:36 +0100

    messenge#

--- okok.py
+++ okok.py
@@ -1,8 +1,7 @@
-import logging
-import os
 from flask import Flask, request
 import pyfiglet
 import sys
+import threading
 
 header = pyfiglet.figlet_format("MESSENGER",font='banner3-D',width=100)
 
@@ -10,10 +9,7 @@ if len(sys.argv) == 2:
     app = Flask(__name__)
 
     print(header)
-    print("Listening on port %s" % str(sys.argv[1]))
-
-    logging.getLogger("werkzeug").disabled =True
-    os.environ["WERKZEUG_RUN_MAIN"] ="True"
+    print("Listening on port {}".format(str(sys.argv[1])))
 
     @app.route('/',methods=['POST'])
     def messener():
@@ -30,9 +26,6 @@ elif len(sys.argv) == 3:
         while True:
             msg = input("Enter Message\n")
             r = request.post(url='http://%s:%s/' % (sys.argv[1],sys.argv[2]),msg = '[%s] - %s\n' % (username,msg))
-    except request.exceptions.ConnectionError as e:
-        print("Server unreachable")
-        print("Details: {}".format(e))
     except KeyboardInterrupt:
         print("Goodbye")
         sys.exit()
