2ca0db1f1a05dded01ce4471611035ddac13aee9
H4rnas <M.pozaroszczyk@outlook.com>
2022-03-01 15:40:24 +0100

    siemanko jest juz git

---
+++ sp.py
@@ -0,0 +1,46 @@
+from flask import Flask, request
+import pyfiglet
+import requests
+import logging
+import sys
+import os
+
+header = pyfiglet.figlet_format("MESSENGER",font='banner3-D',width=100)
+
+if len(sys.argv) == 2:
+    app = Flask(_name_)
+
+    print(header)
+    print("Listening on port {}".format(str(sys.argv[1])))
+
+    #logging.getLogger("werkzeug").disabled = True
+    #os.environ["WERKZEUG_RUN_MAIN"] = "true"
+
+
+    @app.route('/', methods=['POST'])
+    def messenger():
+        request_msg = request.get_data(as_text=True)
+        print(request_msg)
+        return request_msg
+
+
+    app.run(port=sys.argv[1], host='0.0.0.0', use_reloader=False)
+    print('\nGoodbye')
+
+elif len(sys.argv) == 3:
+    try:
+        print(header)
+        username = input("Enter username:")
+        print('Press CTRL + C to exit')
+        while True:
+            data = input('Message: \n')
+            r = requests.post(url='http://%s:%s/' % (sys.argv[1], sys.argv[2]), data='[%s] - %s\n' % (username, data))
+    except requests.exceptions.ConnectionError as e:
+        print('Server is unavalable')
+        print(e)
+    except KeyboardInterrupt:
+        print('\nGoodbye!')
+        sys.exit()
+    except Exception as e:
+        print('Something is wrong')
+        print('Details: {}'.format(e))
